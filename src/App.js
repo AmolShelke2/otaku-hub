687de80f9021b52709ca68d3452d7f4120b5b6ac
Amol <shelkeamol897@gmail.com>
2023-06-18 22:03:23 +0530

    [check]: tailwind working

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="h-full w-full">
+      <h1 className="text-2xl text-center">Hello</h1>
     </div>
   );
 }

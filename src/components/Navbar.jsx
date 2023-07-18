b51bc84b18f7d5ad20f96bd4363d0d3f490df23e
Amol <shelkeamol897@gmail.com>
2023-07-18 23:27:24 +0530

    navbar created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 export const Navbar = () => {
   return (
-    <div>
-      <h1 className=" text-2xl">Navbar</h1>
+    <div className="flex flex-row justify-between px-4 py-2">
+      <div className="px-4">
+        <h1 className="text-lg">Anime hub</h1>
+      </div>
+      <div>
+        <ul className="flex flex-row gap-3">
+          <li>Home</li>
+          <li>Top series</li>
+          <li>Recent watched</li>
+          <li>About us</li>
+        </ul>
+      </div>
     </div>
   );
 };
